e71216f7c08265345a1c5c9ad0829fc054e97c06
Administrator <admin@example.com>
2022-02-21 10:35:27 +0100

    Remove useless localstack compose file

--- iac/layers/user/_outputs.tf
+++
@@ -1,8 +0,0 @@
-
-output "user_arns" {
-  value = values(aws_iam_user.user)[*].name
-}
-
-output "user_names" {
-  value = values(aws_iam_user.user)[*].arn
-}
--- iac/modules/aws_ec2_application/_outputs.tf
+++
@@ -1,4 +0,0 @@
-
-output "web_server_public_ip" {
-  value = aws_instance.server.public_ip
-}
