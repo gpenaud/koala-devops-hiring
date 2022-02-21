ecac782cc84f0675e9ca9a3fae33540ff6f4d363
Administrator <admin@example.com>
2022-02-21 17:52:10 +0100

    Replace amazon image by ubuntu image

--- layers/codepipeline/main.tf
+++ layers/codepipeline/main.tf
@@ -98,7 +98,7 @@ resource "aws_codebuild_project" "webapp_build" {
 
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:2.0"
+    image                       = "aws/codebuild/standard:5.0"
     image_pull_credentials_type = "CODEBUILD"
     privileged_mode             = false
     type                        = "LINUX_CONTAINER"
